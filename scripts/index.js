d15348dded6700c408c12f872fec0540331b54b5
hydapig <moh8287@gmail.com>
2025-05-08 21:13:35 -0600

    updated css to general format of example provided.

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,11 @@ let year = date.getFullYear();
 const formattedDate = date.toLocaleString('en-US', { timeZoneName: 'short' });
 document.getElementById('year').innerHTML = year
 document.getElementById('lastModified').innerHTML = formattedDate
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
