eed24b7c953573ed287d8acc08922dc0298ad658
hydapig <moh8287@gmail.com>
2025-06-05 23:05:11 -0600

    created final project site plan

---
+++ final/scripts/site-plan.js
@@ -0,0 +1,5 @@
+const date = new Date();
+let year = date.getFullYear();
+const formattedDate = date.toLocaleString('en-US', { timeZoneName: 'short' });
+document.getElementById('year').innerHTML = year
+document.getElementById('lastModified').innerHTML = formattedDate
